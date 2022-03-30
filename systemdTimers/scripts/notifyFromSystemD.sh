d6e5b4a8102859ccbab7bed4cbd6de4bced7182d
Tomas Krulis <krulis.tomas.tk@gmail.com>
2022-03-30 18:52:56 +0200

    WIP systemd notification service. Execution 3 minutes after boot is working, but the notification is not sent to the desktop ...

---
+++ systemdTimers/scripts/notifyFromSystemD.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Variable to hold path to systemd job logs
+SYSTEMD_LOG_DIR='/home/tomaskrulis/scheduledJobLogs/systemDJobLogs'
+SYSTEMD_JOB_NAME='NotifySystemD'
+CURRENT_MONTH=$(date '+%b')
+
+# Send notification to desktop
+notify-send 'You can automate and schedule anything with systemd today!'
+
+# Write down in the log
+CURRENT_TIME=$(date '+%Y-%m-%d-%H:%M')
+LOG_RECORD="${CURRENT_TIME} SystemD notification job executed."
+
+# Create directory for cron jobs logging, if it doesn't already exist. And don't error if it does exist
+mkdir -p $SYSTEMD_LOG_DIR/$SYSTEMD_JOB_NAME
+
+# Write down the greeting!
+echo $LOG_RECORD >> $SYSTEMD_LOG_DIR/$SYSTEMD_JOB_NAME/$CURRENT_MONTH.txt
