b4fb6cbed35f98845b7438cd52ae30d760086c5a
Tomas Krulis <krulis.tomas.tk@gmail.com>
2022-07-27 23:21:02 +0200

    Updated example home-manager configuration for nixos.

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -371,6 +371,20 @@
     home.packages = with pkgs; [
       alacritty
     ];
+
+    # Test of writing specific configuration files with home-manager
+    home.file = { # $HOME path is prepended before the file path. There is no need to write `~/`
+      "nix-write-file-test/test.txt".text =
+        ''
+        This is a test of { special } symbols # and this too \ ...
+        '';
+    };
+
+    home.file.".doom.d" = { # this could be used for .doom.d for example
+      source = /home/tomaskrulis/Documents/MegaSynchonized/MegaSynchronized/configs/emacs/DoomEmacs/.doom.d;
+      recursive = true;
+      # executable = true; # useful for scripts
+    };
   };
 
   # List services that you want to enable:
