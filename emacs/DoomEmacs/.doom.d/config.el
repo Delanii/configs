84870ddf712b5cc15741704abe687e095bae69d0
Tomas Krulis <krulis.tomas@seznam.cz>
2022-12-04 22:13:54 +0100

    Updated the emacs source folder.

--- emacs/DoomEmacs/.doom.d/config.el
+++ emacs/DoomEmacs/.doom.d/config.el
@@ -32,7 +32,7 @@
 
 ;; Setting location of emacs source code to access documentation for functions that are written in C:
 ;;
-(setq source-directory (concat (getenv "HOME") "/emacs-projects/emacsSource/emacs"))
+(setq source-directory (concat (getenv "HOME") "/emacsBuild/emacs"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Allow all commands
